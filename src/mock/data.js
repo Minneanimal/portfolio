73e940300d335daad0fefd2c0ef897fa1624b3c9
Brandon Kiel <brandonrkiel@gmail.com>
2021-12-07 07:45:21 -0600

    added images and project summary

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,4 +1,5 @@
 import { nanoid } from 'nanoid';
+import resume from '../bkiel.pdf';
 
 // HEAD DATA
 export const headData = {
@@ -24,35 +25,38 @@ export const aboutData = {
     'I have a demonstrated track record of effectively building complex and scalable business applications using industry standard Agile frameworks like scrum, kanban, and SAFe',
   paragraphThree:
     'I have experience managing and build enterprise applications on all 3 major cloud service providers.',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: resume, // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: 'UFOllow',
-    info: '',
-    info2: '',
+    img: 'pexels-miriam-espacio-365625.jpg',
+    title: 'UFOllow WIP',
+    info: 'The idea for this project occured to me after reading a reddit post in which the author was asking if anyone had seen strange lights in the sky at a particular location.',
+    info2:
+      'The goal of this application is to provide a Waze like experience for folks who see unexplaned lights in the sky.',
     url: '',
     repo: 'https://github.com/Minneanimal/u-follow', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: 'Come At Me Bro',
-    info: '',
-    info2: '',
+    img: 'pexels-koolshooters-8544675.jpg',
+    title: 'Come At Me Bro WIP',
+    info: 'I am a compettive person by nature. The idea of a competitive push-up app came after a friend of mine and gym partner wanted to keep track of who had the highest reps in the gym on that particular day.',
+    info2:
+      'The goal of this application is to provide the mechanisms to track a timed push-up competion in realtime.',
     url: '',
     repo: 'https://github.com/Minneanimal/come-at-me-bro', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: 'Gimli',
-    info: '',
-    info2: '',
+    img: 'pexels-dnd-dice.jpg',
+    title: 'Gimli WIP',
+    info: 'The idea of Gimli came about after I discovered the concept of play-by-post role playing games. Prior to popularity of tabletop role playing games like Dungeons and Dragons, folks used to use forum posts a means to play DnD',
+    info2:
+      'The goal of this app is to take the idea of play-by-post and translate it to a modern web stack.',
     url: '',
     repo: 'https://github.com/Minneanimal/Gimli', // if no repo, the button will not show up
   },
